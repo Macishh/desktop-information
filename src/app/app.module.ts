d65ba97825c77881801dc3a3f2bc7344b1bf5147
Macishh <marcusaxelsson52@gmail.com>
2020-11-10 21:23:02 +0100

    added component for welcome, weather and transport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { WeatherComponent } from './weather/weather.component';
+import { TransportComponent } from './transport/transport.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    WelcomeComponent,
+    WeatherComponent,
+    TransportComponent
   ],
   imports: [
     BrowserModule,
