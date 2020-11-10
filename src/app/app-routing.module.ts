76c094fbd101796a6c59426cac1f823b94164c7e
Macishh <marcusaxelsson52@gmail.com>
2020-11-10 21:28:10 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TransportComponent } from './transport/transport.component';
+import { WeatherComponent } from './weather/weather.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: WelcomeComponent},
+  {path: "weather", component: WeatherComponent},
+  {path: "transport", component: TransportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
