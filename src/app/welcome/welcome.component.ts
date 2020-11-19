ed0fe97baf5f72dd13439249b5435934a835a937
Macishh <marcusaxelsson52@gmail.com>
2020-11-19 21:56:28 +0100

    added functionality for random daily quote

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -18,15 +18,15 @@ export class WelcomeComponent implements OnInit {
   getDailyQuote() {
     fetch('https://type.fit/api/quotes')
     .then(response=>response.json())
-    .then(data=>{this.setDailyQuote(data);})
-
-    //this.setDailyQuote('{     "text": "Genius is one percent inspiration and ninety-nine percent perspiration.",     "author": "Thomas Edison"   },   {     "text": "You can observe a lot just by watching.",     "author": "Yogi Berra"   },   {     "text": "A house divided against itself cannot stand.",     "author": "Abraham Lincoln"   },   {     "text": "Difficulties increase the nearer we get to the goal.",     "author": "Johann Wolfgang von Goethe"  }')
+    .then(data=>{this.setQuoteArray(data);})
   }
 
-  setDailyQuote(data) {
-    console.log(data)
-    this.DailyQuote = data;
-    console.log(this.DailyQuote)
+  setQuoteArray(data) {
+    let rand = Math.floor(Math.random() * data.length);
+    this.DailyQuote = data[rand];
+    console.log(rand);
+    console.log(data.length);
+    console.log(this.DailyQuote);
   }
 
 }
